a96c8c781e3777f5cbd4cb0174e17a18f87bfeba
Keumbi Park <rmaqldi@gmail.com>
2022-05-23 02:49:05 +0900

    Add files via upload

---
+++ bg.js
@@ -0,0 +1,26 @@
+const images = [
+  "1.jpg",
+  "2.jpg",
+  "3.jpg",
+  "4.jpg",
+  "5.jpg",
+  "6.jpg",
+  "7.jpg",
+  "8.jpg",
+  "9.jpg",
+  "10.jpg",
+  "11.jpg",
+  "12.jpg",
+  "13.jpg",
+  "14.jpg",
+  "15.jpg",
+  "16.jpg",
+  "17.jpg",
+  "18.jpg",
+  "19.jpg",
+  "20.jpg",
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url(./img/${chosenImage})`;
